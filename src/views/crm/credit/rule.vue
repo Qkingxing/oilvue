377f533c5beb9c7956fbdd6be8c05567b8966572
pandegong <674948122@qq.com>
2021-01-16 16:29:19 +0800

    111

--- src/views/crm/credit/rule.vue
+++ src/views/crm/credit/rule.vue
@@ -1,12 +1,177 @@
 
 <template>
-  <div>
-      rule
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ margin: '24px 0', padding: '0 24px 24px 24px', background: '#fff', minHeight: '280px', position: 'relative' }">
+      <div class="addRuleBtn">
+        <a-button type="primary">
+          新增积分规则
+        </a-button>
+      </div>
+      <a-tabs default-active-key="1" size="large">
+        <a-tab-pane key="1" tab="进行中">
+          <s-table
+            ref="table"
+            size="default"
+            rowKey="key"
+            :columns="columns"
+            :data="loadData"
+          >
+            <span slot="action" slot-scope="text, record">
+              <template>
+                <a @click="delTag(record)">撤回</a>
+              </template>
+            </span>
+          </s-table>
+        </a-tab-pane>
+        <a-tab-pane key="2" tab="待开始">
+          <s-table
+            ref="table"
+            size="default"
+            rowKey="key"
+            :columns="columns"
+            :data="loadData"
+          >
+            <span slot="action" slot-scope="text, record">
+              <template>
+                <a @click="delTag(record)">撤回</a>
+              </template>
+            </span>
+          </s-table>
+        </a-tab-pane>
+        <a-tab-pane key="3" tab="已结束">
+          <s-table
+            ref="table"
+            size="default"
+            rowKey="key"
+            :columns="columns"
+            :data="loadData"
+          >
+            <span slot="action" slot-scope="text, record">
+              <template>
+                <a @click="delTag(record)">撤回</a>
+              </template>
+            </span>
+          </s-table>
+        </a-tab-pane>
+      </a-tabs>
+    </a-layout-content>
+
+  </a-layout>
 </template>
 
 <script>
+import { STable } from '@/components'
+
+import { getRoleList, getServiceList } from '@/api/manage'
+
 export default {
-    name: 'rule'
+  name: 'rule',
+  components: {
+    STable
+  },
+  data () {
+    return {
+      // 表头
+      columns: [
+        {
+          title: '文件名称',
+          dataIndex: 'no'
+        },
+        {
+          title: '操作人',
+          dataIndex: 'description'
+        },
+        {
+          title: '成功数',
+          dataIndex: 'status',
+          needTotal: true
+        },
+        {
+          title: '失败数',
+          dataIndex: 'time',
+          needTotal: true
+        },
+        {
+          title: '总积分',
+          // dataIndex: 'status',
+          needTotal: true
+        },
+        {
+          title: '总余额',
+          // dataIndex: 'status',
+          needTotal: true
+        },
+        {
+          title: '加油卡名称',
+          // dataIndex: 'status',
+          needTotal: true
+        },
+        {
+          title: '导入状态',
+          // dataIndex: 'status',
+          needTotal: true
+        },
+        {
+          title: '导入时间',
+          // dataIndex: 'status',
+          needTotal: true
+        },
+        {
+          title: '操作',
+          dataIndex: 'action',
+          scopedSlots: { customRender: 'action' }
+        }
+      ],
+      // 加载数据方法 必须为 Promise 对象
+      loadData: parameter => {
+        console.log('loadData.parameter', parameter)
+        return getServiceList(Object.assign(parameter, this.queryParam))
+          .then(res => {
+            return res.result
+          })
+      },
+      selectedRowKeys: [],
+      selectedRows: [],
+
+      // custom table alert & rowSelection
+      options: {
+        rowSelection: {
+          selectedRowKeys: this.selectedRowKeys,
+          onChange: this.onSelectChange
+        }
+      },
+      optionAlertShow: false
+    }
+  },
+  created () {
+    this.tableOption()
+    getRoleList({ t: new Date() })
+  },
+  methods: {
+    tableOption () {
+      if (!this.optionAlertShow) {
+        this.options = {
+          rowSelection: {
+            selectedRowKeys: this.selectedRowKeys,
+            onChange: this.onSelectChange
+          }
+        }
+        this.optionAlertShow = true
+      } else {
+        this.options = {
+          rowSelection: null
+        }
+        this.optionAlertShow = false
+      }
+    }
+  }
 }
 </script>
+<style lang="less" scoped>
+.addRuleBtn{
+  position: absolute;
+  right: 24px;
+  top: 12px;
+  z-index: 70;
+}
+</style>
--- src/views/crm/credit/setting.vue
+++ src/views/crm/credit/setting.vue
@@ -1,12 +1,132 @@
 
 <template>
-  <div>
-      setting
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ margin: '24px 0', padding: '0 24px 24px 24px', background: '#fff', minHeight: '280px' }">
+      <div class="list">
+        <div class="page-block">
+          <div class="head-title">
+            积分设置
+          </div>
+          <div class="deploy-item">
+            <div class="label">积分过期</div>
+            <a-radio-group v-model="value">
+              <a-radio :style="radioStyle" :value="1">
+                统一过期，每年&nbsp;&nbsp;
+                <a-date-picker />
+              </a-radio>
+              <a-radio :style="radioStyle" :value="2">
+                永久有效
+              </a-radio>
+
+            </a-radio-group>
+          </div>
+          <div class="deploy-item">
+            <div class="label">享受优惠不可获积分</div>
+            <a-checkbox-group
+              v-model="checkboxValue"
+              name="checkboxgroup"
+              :options="plainOptions"
+            />
+          </div>
+          <div class="btn-box">
+            <a-button type="primary"> 修改 </a-button>
+          </div>
+        </div>
+      </div>
+    </a-layout-content>
+
+  </a-layout>
 </template>
 
 <script>
+
+const plainOptions = ['优惠券优惠', '会员等级优惠', '价立减优惠', '满减优惠']
+
 export default {
-    name: 'setting'
+  name: 'setting',
+  components: {
+
+  },
+  data () {
+    return {
+      value: 1,
+      plainOptions,
+      checkboxValue: plainOptions,
+      radioStyle: {
+        display: 'block',
+        height: '40px',
+        lineHeight: '40px',
+        marginBottom: '20px'
+      }
+    }
+  },
+  created () {
+
+  },
+  methods: {
+  }
 }
 </script>
+<style lang="less" scoped>
+
+.head-title{
+  font-size: 16px;
+  font-weight: 700;
+  color: #1e1e28;
+  height: 55px;
+  line-height: 41px;
+  border-bottom: 1px solid #eaeaf4;
+  line-height: 60px;
+  margin-bottom: 50px;
+}
+.deploy-item{
+  display: flex;
+  .label{
+    width: 126px;
+    text-align: right;
+    margin-right: 20px;
+    line-height: 40px;
+  }
+  .ant-checkbox-group{
+    line-height: 40px;
+  }
+}
+.btn-box{
+  margin-top: 50px;
+  padding-left: 146px;
+}
+.page-content{
+  margin-top: 30px;
+}
+.growth-setting{
+  .setting-item{
+    display: flex;
+    .setting-title{
+      line-height: 45px;
+      width: 128px;
+      margin-right: 20px;
+      text-align: right;
+    }
+    .setting-content{
+      line-height: 45px;
+    }
+  }
+}
+.growth-level{
+  margin-top: 15px;
+  .level-title{
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 60px;
+    color: #1e1e28;
+  }
+}
+.page-foot{
+  text-align: center;
+  margin-top: 50px;
+  button{
+    margin-right: 20px;
+  }
+}
+
+</style>
