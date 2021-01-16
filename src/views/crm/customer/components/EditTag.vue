cfca464b34a99b36eefe97e61266fd91276506d3
pandegong <674948122@qq.com>
2021-01-16 14:30:05 +0800

    pan

---
+++ src/views/crm/customer/components/EditTag.vue
@@ -0,0 +1,119 @@
+<template>
+  <a-modal v-model="visible" :title="title" on-ok="handleOk" :width="600">
+    <div v-if="type == 'all'" class="box">
+      <div class="tip">已选择「146」位客户</div>
+      <div class="modal-box">
+        <a-form layout="inline">
+          <a-row>
+            <a-form-item label="标签名称">
+              <a-input :maxLength="10" placeholder="请输入标签名称" style="width:300px;"/>
+              <span style="margin-left: -45px; color: rgb(199, 199, 199); position: relative;">10/10</span>
+            </a-form-item>
+          </a-row>
+        </a-form>
+      </div>
+    </div>
+    <div v-if="type == 'add'" class="box">
+
+      <a-form layout="inline">
+        <a-row>
+          <a-form-item label="标签名称">
+            <a-input :maxLength="10" placeholder="请输入标签名称" style="width:300px;"/>
+            <span style="margin-left: -45px; color: rgb(199, 199, 199); position: relative;">10/10</span>
+          </a-form-item>
+        </a-row>
+      </a-form>
+
+    </div>
+    <div v-if="type == 'edit'" class="box">
+
+      <a-form layout="inline">
+        <a-row style="text-align:left;">
+          <a-form-item label="客户编号">
+            <span>1062610</span>
+          </a-form-item>
+        </a-row>
+        <a-row style="text-align:left;">
+          <a-form-item label="客户标签">
+            <a-select placeholder="请选择标签" mode="multiple" style="width: 264px">
+              <a-select-option value="jack">
+                Jack
+              </a-select-option>
+              <a-select-option value="lucy">
+                Lucy
+              </a-select-option>
+              <a-select-option value="disabled" disabled>
+                Disabled
+              </a-select-option>
+              <a-select-option value="Yiminghe">
+                yiminghe
+              </a-select-option>
+            </a-select>
+          </a-form-item>
+        </a-row>
+      </a-form>
+
+    </div>
+    <template slot="footer">
+      <a-button key="back" @click="handleCancel"> 取消 </a-button>
+      <a-button key="submit" type="primary" :loading="loading" @click="handleOk" v-if="type=='all'"> 确认 </a-button>
+      <a-button key="submit" type="primary" :loading="loading" @click="handleOk" v-if="type=='edit'"> 保存 </a-button>
+      <a-button key="submit" type="primary" :loading="loading" @click="handleOk" v-if="type=='add'"> 确认 </a-button>
+    </template>
+  </a-modal>
+</template>
+
+<script>
+export default {
+  name: 'EditTag',
+  data () {
+    return {
+      title: null,
+      type: null,
+      visible: false,
+      loading: false
+    }
+  },
+  methods: {
+    show (type) {
+      this.type = type
+      switch (type) {
+        case 'all':
+          this.title = '批量加标签'
+          break
+        case 'add':
+          this.title = '新增标签'
+          break
+        case 'edit':
+          this.title = '修改标签'
+          break
+        default:
+          break
+      }
+      this.visible = true
+    },
+    handleCancel () {
+      this.visible = false
+    },
+    handleOk () {
+      this.visible = false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.box{
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 14px;
+  line-height: 1.5;
+  text-align: center;
+  .modal-box{
+    min-height: 120px;
+    padding-top: 20px;
+  }
+}
+</style>
--- src/views/crm/customer/list.vue
+++ src/views/crm/customer/list.vue
@@ -8,7 +8,184 @@
           <a-radio-button value="new"> 新增客户 </a-radio-button>
         </a-radio-group>
       </div>
-      <div class="screen-box" v-if="radioValue=='new'">
+      <div class="screen-box" v-if="radioValue=='old'">
+        <a-form layout="inline" >
+          <a-row :gutter="48">
+            <a-col :md="12" :sm="24">
+              <a-form-item label="客户搜索" class="screen-item">
+                <!-- <a-input v-model="queryParam.keywords" placeholder="请输入搜索内容" /> -->
+                <a-input
+                  style="width: 100%"
+                  v-model="queryParam.keywords"
+                  placeholder="请输入搜索内容"
+                >
+                  <a-select
+                    slot="addonBefore"
+                    style="width: 120px"
+                    v-model="queryParam.keyType"
+                  >
+                    <a-select-option value="86"> 客户子单编号 </a-select-option>
+                    <a-select-option value="87"> 手机号 </a-select-option>
+                  </a-select>
+                </a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :md="12" :sm="24">
+              <a-form-item label="是否消费" class="screen-item">
+                <a-select default-value="lucy" style="width: 264px">
+                  <a-select-option value="jack">
+                    Jack
+                  </a-select-option>
+                  <a-select-option value="lucy">
+                    Lucy
+                  </a-select-option>
+                  <a-select-option value="disabled" disabled>
+                    Disabled
+                  </a-select-option>
+                  <a-select-option value="Yiminghe">
+                    yiminghe
+                  </a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="48">
+            <a-col :md="12" :sm="24">
+              <a-form-item label="客户标签" class="screen-item">
+                <a-select default-value="lucy" style="width: 264px">
+                  <a-select-option value="jack">
+                    Jack
+                  </a-select-option>
+                  <a-select-option value="lucy">
+                    Lucy
+                  </a-select-option>
+                  <a-select-option value="disabled" disabled>
+                    Disabled
+                  </a-select-option>
+                  <a-select-option value="Yiminghe">
+                    yiminghe
+                  </a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+            <a-col :md="12" :sm="24">
+              <a-form-item label="偏好油品" class="screen-item">
+                <a-select default-value="lucy" style="width: 264px">
+                  <a-select-option value="jack">
+                    Jack
+                  </a-select-option>
+                  <a-select-option value="lucy">
+                    Lucy
+                  </a-select-option>
+                  <a-select-option value="disabled" disabled>
+                    Disabled
+                  </a-select-option>
+                  <a-select-option value="Yiminghe">
+                    yiminghe
+                  </a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="48">
+            <a-col :md="12" :sm="24">
+              <a-form-item label="会员等级" class="screen-item">
+                <a-select default-value="lucy" style="width: 264px">
+                  <a-select-option value="jack">
+                    Jack
+                  </a-select-option>
+                  <a-select-option value="lucy">
+                    Lucy
+                  </a-select-option>
+                  <a-select-option value="disabled" disabled>
+                    Disabled
+                  </a-select-option>
+                  <a-select-option value="Yiminghe">
+                    yiminghe
+                  </a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+            <a-col :md="12" :sm="24">
+              <a-form-item label="客户身份" class="screen-item">
+                <a-select default-value="lucy" style="width: 264px">
+                  <a-select-option value="jack">
+                    Jack
+                  </a-select-option>
+                  <a-select-option value="lucy">
+                    Lucy
+                  </a-select-option>
+                  <a-select-option value="disabled" disabled>
+                    Disabled
+                  </a-select-option>
+                  <a-select-option value="Yiminghe">
+                    yiminghe
+                  </a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="48">
+            <a-col :md="12" :sm="24">
+              <a-form-item label="最近加油时间" class="screen-item">
+                <a-range-picker />
+              </a-form-item>
+            </a-col>
+            <a-col :md="12" :sm="24">
+              <a-form-item label="加油升数" class="screen-item">
+                <a-input-group compact>
+                  <a-input style="width:88px;" placeholder="数字"/>
+                  <span style="margin: 0px 8px;line-height:32px;">至</span>
+                  <a-input style="width:88px;" placeholder="数字"/>
+                  <span style="margin: 0px 8px;line-height:32px;">升</span>
+                </a-input-group>
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="48">
+            <a-col :md="12" :sm="24">
+              <a-form-item label="加油次数" class="screen-item">
+                <a-input-group compact>
+                  <a-input style="width:88px;" placeholder="数字"/>
+                  <span style="margin: 0px 8px;line-height:32px;">至</span>
+                  <a-input style="width:88px;" placeholder="数字"/>
+                  <span style="margin: 0px 8px;line-height:32px;">次</span>
+                </a-input-group>
+              </a-form-item>
+            </a-col>
+            <a-col :md="12" :sm="24">
+              <a-form-item label="现有积分" class="screen-item">
+                <a-input-group compact>
+                  <a-input style="width:88px;" placeholder="数字"/>
+                  <span style="margin: 0px 8px;line-height:32px;">至</span>
+                  <a-input style="width:88px;" placeholder="数字"/>
+                  <span style="margin: 0px 8px;line-height:32px;">分</span>
+                </a-input-group>
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="48">
+            <a-col :md="12" :sm="24">
+              <a-form-item label="加油卡余额" class="screen-item">
+                <a-input-group compact>
+                  <a-input style="width:88px;" placeholder="数字"/>
+                  <span style="margin: 0px 8px;line-height:32px;">至</span>
+                  <a-input style="width:88px;" placeholder="数字"/>
+                  <span style="margin: 0px 8px;line-height:32px;">元</span>
+                </a-input-group>
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row :gutter="48">
+            <a-col :md="24" :sm="24">
+              <a-form-item>
+                <a-button type="primary" class="search-btn" style="min-width:82px;"> 搜索 </a-button>
+              </a-form-item>
+            </a-col>
+          </a-row>
+        </a-form>
+      </div>
+      <div class="screen-box" v-else>
         <a-form layout="inline" >
           <a-row :gutter="48">
             <a-col :md="24" :sm="24">
@@ -49,11 +226,16 @@
                 3
               </span>
               」位
-              <span class="themeColor select-all">
+              <span class="themeColor select-all" @click="showEditTag('all')">
                 选择全部
               </span>
             </span>
           </span>
+          <div>
+            <a-button disabled>发优惠券</a-button>
+            <a-button style="margin-left: 8px;">导出报表</a-button>
+            <a-button style="margin-left: 8px;" icon="setting" v-if="radioValue=='old'"/>
+          </div>
         </div>
         <s-table
           ref="table"
@@ -61,29 +243,36 @@
           rowKey="key"
           :columns="columns"
           :data="loadData"
-          :rowSelection="options.rowSelection"
+          :rowSelection="{ selectedRowKeys: selectedRowKeys, onChange: onSelectChange }"
         >
-          <span slot="serial" slot-scope="text, record, index">
-            {{ index + 1 }}
+          <span slot="action" slot-scope="text, record">
+            <template>
+              <a @click="showEditTag('edit',record)" v-if="radioValue=='old'">修改</a>
+              <a-divider type="vertical" />
+              <a @click="showEditTag('add',record)">加标签</a>
+            </template>
           </span>
         </s-table>
       </div>
     </a-layout-content>
+    <EditTag ref="EditTag"></EditTag>
   </a-layout>
 </template>
 
 <script>
 import { STable } from '@/components'
 import { getRoleList, getServiceList } from '@/api/manage'
+import EditTag from './components/EditTag'
 
 export default {
   name: 'Clist',
   components: {
-    STable
+    STable,
+    EditTag
   },
   data () {
     return {
-      radioValue: 'new',
+      radioValue: 'old',
       // 查询参数
       queryParam: {
         keyType: '86'
@@ -91,38 +280,36 @@ export default {
       // 表头
       columns: [
         {
-          title: '#',
-          scopedSlots: { customRender: 'serial' }
-        },
-        {
-          title: '规则编号',
+          title: '客户子编号',
           dataIndex: 'no'
         },
         {
-          title: '描述',
+          title: '手机号',
           dataIndex: 'description'
         },
         {
-          title: '服务调用次数',
-          dataIndex: 'callNo',
-          sorter: true,
-          needTotal: true,
-          customRender: (text) => text + ' 次'
-        },
-        {
-          title: '状态',
+          title: '消费次数',
           dataIndex: 'status',
           needTotal: true
         },
         {
-          title: '更新时间',
-          dataIndex: 'updatedAt',
-          sorter: true
+          title: '会员等级',
+          // dataIndex: 'status',
+          needTotal: true
         },
         {
-          title: '操作',
+          title: '现有积分',
+          // dataIndex: 'status',
+          needTotal: true
+        },
+        {
+          title: '加油卡余额',
+          // dataIndex: 'status',
+          needTotal: true
+        },
+        {
+          title: '标签',
           dataIndex: 'action',
-          width: '150px',
           scopedSlots: { customRender: 'action' }
         }
       ],
@@ -152,6 +339,9 @@ export default {
     getRoleList({ t: new Date() })
   },
   methods: {
+    showEditTag (type) {
+      this.$refs['EditTag'].show(type)
+    },
     tableOption () {
       if (!this.optionAlertShow) {
         this.options = {
@@ -167,6 +357,11 @@ export default {
         }
         this.optionAlertShow = false
       }
+    },
+    onSelectChange (selectedRowKeys, selectedRows) {
+      console.log()
+      this.selectedRowKeys = selectedRowKeys
+      this.selectedRows = selectedRows
     }
   }
 }
@@ -224,4 +419,5 @@ export default {
   margin-left: 16px;
   cursor: pointer;
 }
+
 </style>
