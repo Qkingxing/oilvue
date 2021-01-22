debb15848c4be7275f7fbf68282121b7e935700c
pandegong <674948122@qq.com>
2021-01-22 22:38:15 +0800

    111

--- src/utils/JavaRequest.js
+++ src/utils/JavaRequest.js
@@ -9,7 +9,7 @@ import { ACCESS_TOKEN } from '@/store/mutation-types'
 const JavaRequest = axios.create({
   // API 请求的默认前缀
   // baseURL: process.env.VUE_APP_API_BASE_URL,
-  baseURL: 'https://oiljava.ldyxx.com',
+  baseURL: 'https://oiljava.ldyxx.com:4435',
   timeout: 6000 // 请求超时时间
 })
 
