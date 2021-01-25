b331de3bafc3c957cc83f6c47e2d0e86efd66abe
每天都要努力 <52522370+caojishuai520520@users.noreply.github.com>
2021-01-25 21:18:32 +0800

    会员 yemian1

---
+++ src/views/dmp/customer/datas.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="shijian">
+        <a-tabs default-active-key="1" @change="callback">
+            <a-tab-pane key="1" tab="今日">
+                <div class="content">
+                    <div class="head_title">
+                        <span class="">会员总数据</span>
+                    </div>
+                    <div class="overflow_box">
+                        <biao></biao>
+                    </div>
+                </div>
+            </a-tab-pane>
+            <a-tab-pane key="2" tab=" 昨日" force-render>
+                昨日
+            </a-tab-pane>
+            <a-tab-pane key="3" tab="7天">
+                7天
+            </a-tab-pane>
+             <a-tab-pane key="4" tab="30天">
+                30天
+            </a-tab-pane>
+             <a-tab-pane key="5" tab="日">
+                日
+            </a-tab-pane>
+             <a-tab-pane key="6" tab="周">
+               周
+            </a-tab-pane>
+             <a-tab-pane key="7" tab="月">
+                月
+            </a-tab-pane>
+        </a-tabs>
+    </div>
+</template>
+
+<script>
+import biao  from '../customer/biao'
+export default {
+    components:{biao},
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        callback(key) {
+         console.log(key);
+        },
+    }
+}
+</script>
+
+<style lang='scss'>
+    .shijian{
+        .content{
+            box-sizing: border-box;
+            .head_title{
+                height: 20px;
+                margin: 20px 0;
+            }
+        }
+    }
+</style>
--- src/views/dmp/customer/member.vue
+++ src/views/dmp/customer/member.vue
@@ -1,12 +1,43 @@
 
 <template>
-  <div>
-    member
+  <div class="member">
+      <div class="mainContainreBlock">
+          <div class="container animated fadeIn">
+              <div class="date-tab-panes">
+                 <datas></datas>
+              </div>
+             
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
+// import { component } from 'node_modules/vue/types/umd'
+import datas from './datas'
 export default {
-    name: 'Member'
+    name: 'Member',
+    components:{datas}
 }
 </script>
+<style lang="scss" scoped>
+    .member{
+        padding-top: 54px;
+        .mainContainreBlock{
+            padding-left: 24px;
+            padding-right: 24px;
+            padding-bottom: 25px;
+            background: #fff;
+            min-width: 1004px;
+            overflow: auto;
+            .container{
+                padding-top: 24px;
+                .date-tab-panes{
+                    position: relative;
+                    padding-top: 5px;
+                    display: flex;
+                }
+            }
+        }
+    }
+</style>
