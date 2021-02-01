e5781297df97b35d84954d6f7f91492d88229a8f
liuyangyang <liuyangyangwork@qq.com>
2021-02-01 16:32:42 +0800

    支付-交易接口完成对接

--- src/api/set.js
+++ src/api/set.js
@@ -16,8 +16,44 @@ function setIdentityset(parameter) {
     data: parameter
   })
 }
+//设置/支付设置/数据返回
+function setPayastrictlist(parameter) {
+  return PhpRequest({
+    url: '/set/payastrictlist',
+    method: 'post',
+    data: parameter
+  })
+}
+//设置/交易管理/支付设置
+function setPayastrictset(parameter){
+  return PhpRequest({
+    url: '/set/payastrictset',
+    method: 'post',
+    data: parameter
+  })
+}
+//设置/支付设置/退款信息返回
+function setRefundastrictlist(parameter){
+  return PhpRequest({
+    url: 'set/refundastrictlist',
+    method: 'post',
+    data: parameter
+  })
+}
+//设置/交易管理/退款限制
+function setRefundastrictset(parameter){
+  return PhpRequest({
+    url: 'set/refundastrictset',
+    method: 'post',
+    data: parameter
+  })
+}
 const api = {
   setIdentitylist,
-  setIdentityset
+  setIdentityset,
+  setPayastrictlist,
+  setPayastrictset,
+  setRefundastrictlist,
+  setRefundastrictset
 }
 export default api
