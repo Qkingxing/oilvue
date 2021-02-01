54011e775a577bbc4f0a37290ca70ac7d27d7414
每天都要努力 <52522370+caojishuai520520@users.noreply.github.com>
2021-02-01 15:30:15 +0800

    修改

--- src/views/dmp/performance/follow.vue
+++ src/views/dmp/performance/follow.vue
@@ -165,6 +165,126 @@
 
 				  
 			  </div>
+			   <div class="ve-card-view" v-if="size == 'yue'">
+				  <div class="title-obj">
+					  数据统计
+					  <div class="dev">
+						<i class="more_info anticon anticon-question-circle">
+							<a-popover  overlayClassName="note">
+								<template slot="content">
+									<div class="" style="width: 200px">
+									<p>数据于每日早10点更新</p>
+									</div>
+								</template>
+								<span class="anticon">
+									<a-icon type="question-circle" />
+								</span>
+							</a-popover>
+						</i>
+					</div>
+				  </div>
+				  <div class="overflow_box">
+					 <labels></labels>
+				  </div>
+				  <div class="ve-pie-view">
+					  <div class="title-obj">
+						  占比图
+							<div class="dev">
+								<i class="more_info anticon anticon-question-circle">
+									<a-popover  overlayClassName="note">
+										<template slot="content">
+											<div class="" style="width: 200px">
+											<p>数据于每日早10点更新</p>
+											</div>
+										</template>
+										<span class="anticon">
+											<a-icon type="question-circle" />
+										</span>
+									</a-popover>
+								</i>
+							</div>
+					  </div>
+					  <div class="pie-box">
+						  <div class="pie-view">
+							  <div class="tap-title">
+								  引导销售收入占比
+							  </div>
+							  <div class="pie-is-null">暂无数据</div>
+						  </div>
+						   <div class="pie-view">
+							  <div class="tap-title">
+								  引导加油量占比
+							  </div>
+							  <div class="pie-is-null">暂无数据</div>
+						  </div>
+					  </div>
+
+					  
+				  </div>
+
+
+				  
+			  </div>
+			   <div class="ve-card-view" v-if="size == 'yue1'">
+				  <div class="title-obj">
+					  数据统计
+					  <div class="dev">
+						<i class="more_info anticon anticon-question-circle">
+							<a-popover  overlayClassName="note">
+								<template slot="content">
+									<div class="" style="width: 200px">
+									<p>数据于每日早10点更新</p>
+									</div>
+								</template>
+								<span class="anticon">
+									<a-icon type="question-circle" />
+								</span>
+							</a-popover>
+						</i>
+					</div>
+				  </div>
+				  <div class="overflow_box">
+					 <labels></labels>
+				  </div>
+				  <div class="ve-pie-view">
+					  <div class="title-obj">
+						  占比图
+							<div class="dev">
+								<i class="more_info anticon anticon-question-circle">
+									<a-popover  overlayClassName="note">
+										<template slot="content">
+											<div class="" style="width: 200px">
+											<p>数据于每日早10点更新</p>
+											</div>
+										</template>
+										<span class="anticon">
+											<a-icon type="question-circle" />
+										</span>
+									</a-popover>
+								</i>
+							</div>
+					  </div>
+					  <div class="pie-box">
+						  <div class="pie-view">
+							  <div class="tap-title">
+								  引导销售收入占比
+							  </div>
+							  <div class="pie-is-null">暂无数据</div>
+						  </div>
+						   <div class="pie-view">
+							  <div class="tap-title">
+								  引导加油量占比
+							  </div>
+							  <div class="pie-is-null">暂无数据</div>
+						  </div>
+					  </div>
+
+					  
+				  </div>
+
+
+				  
+			  </div>
 		  </div>
 	  </div>
   </div>
