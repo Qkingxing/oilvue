a4d264bec87e3abfc366b568d4122a010b51c006
每天都要努力 <52522370+caojishuai520520@users.noreply.github.com>
2021-03-03 14:49:31 +0800

    员工权限管理

--- src/views/oa/staff/power.vue
+++ src/views/oa/staff/power.vue
@@ -14,24 +14,65 @@
                 <div class="roles" v-for="a in 9" :key="a">
                   <template>
                     <a-card hoverable style="width: 300px; margin: 10px 10px">
-                      <a style="padding-left: 190px;display: block;margin-top: -14px;">修改权限</a>
+                      <a style="padding-left: 190px; display: block; margin-top: -14px; color: #37f">修改权限</a>
                       <div style="display: flex; flex-direction: column">
-                        <div style="margin: 32px auto;">
-                          <div style="width:50px;height:50px;border: 1px solid;margin: 0 auto;border-radius: 30px;">
-                          </div>
-                          <div style="margin: 20px 0 30px 0;text-align: center;">加油员</div>
+                        <div style="margin: 32px auto">
+                          <div
+                            style="width: 50px; height: 50px; border: 1px solid; margin: 0 auto; border-radius: 30px"
+                          ></div>
+                          <div style="margin: 20px 0 30px 0; text-align: center">加油员</div>
                         </div>
                       </div>
 
-                      <div style="margin-bottom: 60px;">
-                          <span>权限说明：拥有油站下的全部数据查看</span>
-                          <span>权限及功能修改权限</span>
-                          <p>该角色目前已配置16个账号</p>
+                      <div style="margin-bottom: 60px">
+                        <span>权限说明：拥有油站下的全部数据查看</span>
+                        <span>权限及功能修改权限</span>
+                        <p>该角色目前已配置16个账号</p>
                       </div>
 
-                      <div style="display: flex;justify-content: space-between;">
-                        <p>角色权限<a>详情</a></p>
-                        <p>角色列表<a>详情</a></p>
+                      <div style="display: flex; justify-content: space-between">
+                        <p>
+                          角色权限
+                          <a-popover placement="right">
+                            <template slot="content">
+                              <a-tree show-line :default-expanded-keys="['0-0-0']" @select="onSelect">
+                                <a-icon slot="switcherIcon" type="down" />
+                                <a-tree-node key="0-0" title="商户平台">
+                                  <a-tree-node key="0-0-3" title="首页"> </a-tree-node>
+                                  <a-tree-node key="0-0-0" title="数据">
+                                    <a-tree-node key="0-0-0-0" title="员工绩效" />
+                                    <a-tree-node key="0-0-0-1" title="加油统计" />
+                                    <a-tree-node key="0-0-0-2" title="充值统计" />
+                                    <a-tree-node key="0-0-0-3" title="关注统计" />
+                                  </a-tree-node>
+                                  <a-tree-node key="0-0-1" title="概览">
+                                    <a-tree-node key="0-0-1-0" title="实时概览" />
+                                    <a-tree-node key="0-0-1-1" title="整体看板">
+                                      <a-tree-node key="0-0-2-0" title="查看整体看板" />
+                                      <a-tree-node key="0-0-2-1" title="查看订单概览" />
+                                      <a-tree-node key="0-0-2-2" title="导出报表" />
+                                    </a-tree-node>
+                                  </a-tree-node>
+                                  <a-tree-node key="0-0-2" title="营销">
+                                    <a-tree-node key="0-0-2-0" title="实时概览" />
+                                    <a-tree-node key="0-0-2-1" title="活动统计" />
+                                  </a-tree-node>
+                                </a-tree-node>
+								<a-tree-node key="0-1" title="商户POS端">
+									 <a-tree-node key="0-1-1" title="首页"> </a-tree-node>
+                                </a-tree-node>
+								<a-tree-node key="0-2" title="商户小程序(商户端)">
+									 <a-tree-node key="0-2-1" title="首页"> </a-tree-node>
+                                </a-tree-node>
+								<a-tree-node key="0-3" title="OS">
+									 <a-tree-node key="0-3-1" title="首页"> </a-tree-node>
+                                </a-tree-node>
+                              </a-tree>
+                            </template>
+                            <span style="color: #37f">详情</span>
+                          </a-popover>
+                        </p>
+                        <p>角色列表<a style="color: #37f">详情</a></p>
                       </div>
                     </a-card>
                   </template>
@@ -50,6 +91,11 @@ export default {
   name: 'Operformance',
   data() {
     return {}
+  },
+  methods: {
+    onSelect(selectedKeys, info) {
+      console.log('selected', selectedKeys, info)
+    },
   },
 }
 </script>
@@ -110,7 +156,7 @@ export default {
 }
 .roles {
   display: flex;
-//   flex: 1;
+  //   flex: 1;
   flex-wrap: wrap;
   margin: 0px;
 
@@ -118,7 +164,6 @@ export default {
     height: 400px;
     width: 33%;
     display: flex;
-    
   }
 
   .itemContent {
