22455afae4b39d7d23a91c3cbbf9e7c730d1eaa4
pandegong <674948122@qq.com>
2021-03-08 10:22:18 +0800

    111

--- src/permission.js
+++ src/permission.js
@@ -35,7 +35,7 @@ router.beforeEach((to, from, next) => {
             store.dispatch('GenerateRoutes', { roles }).then(() => {
               // 根据roles权限生成可访问的路由表
               // 动态添加可访问路由表
-              router.selfaddRoutes(store.getters.addRouters)
+              router.addRoutes(store.getters.addRouters)
               // 请求带有 redirect 重定向时，登录自动重定向到该地址
               const redirect = decodeURIComponent(from.query.redirect || to.path)
               if (to.path === redirect) {
