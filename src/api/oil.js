1bc4f82f2b1889342e76d6a76b772a2f2e648b98
pandegong <674948122@qq.com>
2021-03-15 15:35:04 +0800

    11

---
+++ src/api/oil.js
@@ -0,0 +1,18 @@
+import JavaRequest from '@/utils/JavaRequest'
+import PhpRequest from '@/utils/PhpRequest'
+import store from '@/store'
+
+
+// 油品挂牌价-列表
+// 获取油品列表
+export function getSonoillist () {
+  let data = {}
+  if (store.getters.site_id!==(-1)) {
+    data.site_id = store.getters.site_id
+  }
+  return PhpRequest({
+    url: '/set/siteoillist',
+    method: 'post',
+    data
+  })
+}
